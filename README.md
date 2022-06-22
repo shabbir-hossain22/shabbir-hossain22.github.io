8e7cf5cff1a6a6a78cffcf5b383406800e8e263f
istiak04 <istyistiak04@gmail.com>
2022-06-22 17:37:10 +0600

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# React-Portfolio.
-
-### Hi there 👋 I am Shabbir, a data scientist. I love to build new things that excites me a lot.
---
+++ precache-manifest.cfed8fe740bd52670d61cada18b35763.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0aab554de18bb5572b2fc7ad31951b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a11f28b8594251cf2ee7",
+    "url": "/static/css/2.2bd6d7de.chunk.css"
+  },
+  {
+    "revision": "ac5adc96f0bf57a6631d",
+    "url": "/static/css/main.0fcd2a7b.chunk.css"
+  },
+  {
+    "revision": "a11f28b8594251cf2ee7",
+    "url": "/static/js/2.2d8dc40e.chunk.js"
+  },
+  {
+    "revision": "da34feb2510ad8d586433b4295567c14",
+    "url": "/static/js/2.2d8dc40e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac5adc96f0bf57a6631d",
+    "url": "/static/js/main.f38ca8d2.chunk.js"
+  },
+  {
+    "revision": "89356bfa8c1309dff60b",
+    "url": "/static/js/runtime-main.771473fb.js"
+  },
+  {
+    "revision": "dd408ddb0a3bd8d4a58a28cad886317f",
+    "url": "/static/media/1.dd408ddb.png"
+  },
+  {
+    "revision": "9d3eebcf508c6f496f2fda5b36cf6bba",
+    "url": "/static/media/2.9d3eebcf.png"
+  },
+  {
+    "revision": "9b7b0a1175260e390b2abfa75cf00699",
+    "url": "/static/media/3.9b7b0a11.png"
+  },
+  {
+    "revision": "8e122e982e5329c6f4fdd20e2d0f7650",
+    "url": "/static/media/4.8e122e98.png"
+  },
+  {
+    "revision": "c3a2d0ae0641f7fa0bb04d74f327f576",
+    "url": "/static/media/BKB.c3a2d0ae.png"
+  },
+  {
+    "revision": "1547a1df78ef6e6779a8e9444e5625bb",
+    "url": "/static/media/bidemo.1547a1df.png"
+  },
+  {
+    "revision": "7dcbc990e0387431220341806cddac2a",
+    "url": "/static/media/customBI.7dcbc990.png"
+  },
+  {
+    "revision": "c60f28efbfd41bff47e3169be1d99b73",
+    "url": "/static/media/idra.c60f28ef.png"
+  },
+  {
+    "revision": "ee8d6c25d3414a062033b0aa1dc3670e",
+    "url": "/static/media/insurance.ee8d6c25.png"
+  },
+  {
+    "revision": "777bc390a8832d46303cd40faa70cc97",
+    "url": "/static/media/powerBI.777bc390.png"
+  },
+  {
+    "revision": "d8894c784a75c77d8665d890b7124416",
+    "url": "/static/media/responseTime.d8894c78.png"
+  },
+  {
+    "revision": "02b845dae848756f27a326078c11f33e",
+    "url": "/static/media/rossi_2.02b845da.jpg"
+  },
+  {
+    "revision": "4281b11d8ed9500fb8ed6d0dbb09ae80",
+    "url": "/static/media/tableau.4281b11d.png"
+  }
+]);
